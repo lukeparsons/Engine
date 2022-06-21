d3b1c002b0161395bceb06d00c4a30e1411a67ee
Luke Parsons <lukehaparsons@gmail.com>
2022-06-21 18:15:11 +0100

    Simple triangle

--- Engine/Engine/src/core/Main.cpp
+++ Engine/Engine/src/core/Main.cpp
@@ -49,6 +49,11 @@ int main()
 		0.0f, 0.5f, 0.0f
 	};
 
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 
@@ -56,12 +61,73 @@ int main()
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	std::optional<std::string> vertexShaderSource = ReadFile("../Engine/src/shaders/BasicVertex.vertex");
+
+	const char* vss = vertexShaderSource.value().c_str();
+
+	glShaderSource(vertexShader, 1, &vss, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error in vertex shader compilation\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	
+	std::optional<std::string> fragmentShaderSource = ReadFile("../Engine/src/shaders/BasicFragment.fragment");
+
+	const char* fss = fragmentShaderSource.value().c_str();
+
+	glShaderSource(fragmentShader, 1, &fss, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error in fragment shader compilation\n" << infoLog << std::endl;
+	}
+
+	unsigned int shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Error in shader link\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	glBindVertexArray(VAO);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
